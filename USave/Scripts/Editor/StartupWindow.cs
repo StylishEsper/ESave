277b70936d700f6c695ea0a81fa8bffacf83877a
Ahmed Butt <ahmedb.work@gmail.com>
2024-05-13 21:49:56 -0400

    fixed installer window bug

--- USave/Scripts/Editor/StartupWindow.cs
+++ USave/Scripts/Editor/StartupWindow.cs
@@ -11,6 +11,7 @@ using UnityEditor.PackageManager;
 
 namespace Esper.USave.Editor
 {
+    [InitializeOnLoad]
     public class StartupWindow : EditorWindow
     {
         private const string alreadyShownKey = "com.stylishesper.usave.startup_window_shown";
@@ -18,7 +19,11 @@ namespace Esper.USave.Editor
         private static StartupWindow window;
         private static AddRequest request;
 
-        [InitializeOnLoadMethod]
+        static StartupWindow()
+        {
+            EditorApplication.delayCall += ShowOnStartup;
+        }
+
         private static void ShowOnStartup()
         {
             var isAlreadyShown = SessionState.GetBool(alreadyShownKey, false);
@@ -27,15 +32,18 @@ namespace Esper.USave.Editor
                 ShowWelcomeWindow();
                 SessionState.SetBool(alreadyShownKey, true);
             }
+
+            EditorApplication.delayCall -= ShowOnStartup;
         }
 
-        [MenuItem("Window/USave/Install Newtonsoft JSON")]
         public static void ShowWelcomeWindow()
         {
+#if !INSTALLED_NEWTONSOFTJSON
             window = GetWindow<StartupWindow>();
             window.maxSize = new Vector2(500f, 100f);
             window.minSize = window.maxSize;
             window.titleContent = new GUIContent("Newtonsoft JSON Installer");
+#endif
         }
 
         private void OnGUI()
@@ -48,7 +56,8 @@ namespace Esper.USave.Editor
             }
         }
 
-        private void InstallNewtonsoftJSON()
+        [MenuItem("Window/USave/Install Newtonsoft JSON")]
+        private static void InstallNewtonsoftJSON()
         {
             request = Client.Add("com.unity.nuget.newtonsoft-json");
             EditorApplication.update += Progress;
