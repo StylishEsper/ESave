ca077c6f730d951c89606143f60d6f7a5395fdb8
Ahmed Butt <ahmedb.work@gmail.com>
2024-06-03 08:40:44 -0400

    minor editor bug fix

--- ESave/Scripts/Editor/SaveFileSetupDataDrawer.cs
+++ ESave/Scripts/Editor/SaveFileSetupDataDrawer.cs
@@ -13,11 +13,8 @@ namespace Esper.ESave.Editor
     [CustomPropertyDrawer(typeof(SaveFileSetupData))]
     public class SaveFileSetupDataDrawer : PropertyDrawer
     {
-        private float propertyHeight;
-
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            propertyHeight = EditorGUIUtility.singleLineHeight;
             float y = position.y;
 
             var headerLineRect = new Rect(position.x, y, position.width, EditorGUIUtility.singleLineHeight);
@@ -32,31 +29,26 @@ namespace Esper.ESave.Editor
                 float x = position.x + indent;
 
                 y += lineSpacing;
-                propertyHeight += lineSpacing;
                 var fileNameRect = new Rect(x, y, position.width - indent, EditorGUIUtility.singleLineHeight);
                 var fileName = property.FindPropertyRelative("fileName");
                 fileName.stringValue = EditorGUI.TextField(fileNameRect, "File Name", fileName.stringValue);
 
                 y += lineSpacing;
-                propertyHeight += lineSpacing;
                 var saveLocationRect = new Rect(x, y, position.width - indent, EditorGUIUtility.singleLineHeight);
                 var saveLocation = property.FindPropertyRelative("saveLocation");
                 saveLocation.enumValueIndex = EditorGUI.Popup(saveLocationRect, "Save Location", saveLocation.enumValueIndex, saveLocation.enumDisplayNames);
 
                 y += lineSpacing;
-                propertyHeight += lineSpacing;
                 var filePathRect = new Rect(x, y, position.width - indent, EditorGUIUtility.singleLineHeight);
                 var filePath = property.FindPropertyRelative("filePath");
                 filePath.stringValue = EditorGUI.TextField(filePathRect, "File Path", filePath.stringValue);
 
                 y += lineSpacing;
-                propertyHeight += lineSpacing;
                 var fileTypeRect = new Rect(x, y, position.width - indent, EditorGUIUtility.singleLineHeight);
                 var fileType = property.FindPropertyRelative("fileType");
                 fileType.enumValueIndex = EditorGUI.Popup(fileTypeRect, "File Type", fileType.enumValueIndex, fileType.enumDisplayNames);
 
                 y += lineSpacing;
-                propertyHeight += lineSpacing;
                 var encryptionMethodRect = new Rect(x, y, position.width - indent, EditorGUIUtility.singleLineHeight);
                 var encryptionMethod = property.FindPropertyRelative("encryptionMethod");
                 encryptionMethod.enumValueIndex = EditorGUI.Popup(encryptionMethodRect, "Encryption Method", encryptionMethod.enumValueIndex, encryptionMethod.enumDisplayNames);
@@ -64,26 +56,22 @@ namespace Esper.ESave.Editor
                 if (encryptionMethod.enumValueIndex == 1)
                 {
                     y += lineSpacing;
-                    propertyHeight += lineSpacing;
                     var aesKeyRect = new Rect(x, y, position.width - indent, EditorGUIUtility.singleLineHeight);
                     var aesKey = property.FindPropertyRelative("aesKey");
                     aesKey.stringValue = EditorGUI.TextField(aesKeyRect, "AES Key", aesKey.stringValue);
 
                     y += lineSpacing;
-                    propertyHeight += lineSpacing;
                     var aesIVRect = new Rect(x, y, position.width - indent, EditorGUIUtility.singleLineHeight);
                     var aesIV = property.FindPropertyRelative("aesIV");
                     aesIV.stringValue = EditorGUI.TextField(aesIVRect, "AES IV", aesIV.stringValue);
                 }
 
                 y += lineSpacing;
-                propertyHeight += lineSpacing;
                 var addToStorageRect = new Rect(x, y, position.width - indent, EditorGUIUtility.singleLineHeight);
                 var addToStorage = property.FindPropertyRelative("addToStorage");
                 addToStorage.boolValue = EditorGUI.Toggle(addToStorageRect, "Add to Storage", addToStorage.boolValue);
 
                 y += lineSpacing;
-                propertyHeight += lineSpacing;
                 var backgroundTaskRect = new Rect(x, y, position.width - indent, EditorGUIUtility.singleLineHeight);
                 var backgroundTask = property.FindPropertyRelative("backgroundTask");
                 backgroundTask.boolValue = EditorGUI.Toggle(backgroundTaskRect, "Background Task", backgroundTask.boolValue);
@@ -94,6 +82,20 @@ namespace Esper.ESave.Editor
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
+            var propertyHeight = EditorGUIUtility.singleLineHeight;
+
+            if (property.isExpanded)
+            {
+                float lineSpacing = EditorGUIUtility.singleLineHeight + 5;
+                propertyHeight = lineSpacing * 8;
+
+                var encryptionMethod = property.FindPropertyRelative("encryptionMethod");
+                if (encryptionMethod.enumValueIndex == 1)
+                {
+                    propertyHeight += lineSpacing * 2;
+                }
+            }
+
             return propertyHeight;
         }
     }
